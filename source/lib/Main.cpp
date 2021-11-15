fae555f4c61e49807dd7133990f24bb239ad9d84
Curle <curle@gemwire.uk>
2021-11-15 20:21:30 +0000

    Fix out-of-bounds read.

--- source/lib/Main.cpp
+++ source/lib/Main.cpp
@@ -25,13 +25,23 @@ extern "C" void Java_java_vm_Printer_println_Ljava_lang_StringE_V_() {
     Variable* data = VM::GetObject(charArray);
     data++; // Skip the char marker
 
-    size_t datalen = VM::GetArrayLength(charArray) + 1;
+    size_t datalen = VM::GetArrayLength(charArray);
 
-    char text[datalen];
+    char text[datalen + 1];
     for(size_t i = 0; i < datalen; i++)
         text[i] = data[i].charVal;
+    text[datalen] = '\0';
 
     printf("%s\n", text);
     
+    VM::Return(10);
+}
+
+
+extern "C" void Java_java_vm_Printer_printint_I_V_() {
+    auto Parameters = VM::GetParameters();
+
+    printf("%d\n", Parameters.at(0).intVal);
+
     VM::Return(10);
 }
